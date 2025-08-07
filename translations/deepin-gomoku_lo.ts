4cbde6995ee4298686783e77c4321bda72a15b58
Tian Shilin <tianshilin@uniontech.com>
2025-08-07 16:01:44 +0800

    feat: Update translations for Lao locale
    log: Translation update

---
+++ translations/deepin-gomoku_lo.ts
@@ -0,0 +1,148 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lo">
+<context>
+    <name>BTMusicControl</name>
+    <message>
+        <location filename="../gomoku/src/widget/buttonfunction/btmusiccontrol.cpp" line="79"/>
+        <source>Sound Off</source>
+        <translation>សອນ្ទរកាយ</translation>
+    </message>
+    <message>
+        <location filename="../gomoku/src/widget/buttonfunction/btmusiccontrol.cpp" line="79"/>
+        <source>Sound On</source>
+        <translation>ສຽງ-on</translation>
+    </message>
+</context>
+<context>
+    <name>BTReplay</name>
+    <message>
+        <location filename="../gomoku/src/widget/buttonfunction/btreplay.cpp" line="83"/>
+        <source>New Game</source>
+        <translation>ເລ่น្តើបថ្មើរ</translation>
+    </message>
+</context>
+<context>
+    <name>BTStartPause</name>
+    <message>
+        <location filename="../gomoku/src/widget/buttonfunction/btstartpause.cpp" line="106"/>
+        <source>Start</source>
+        <translation>ចាប់ផ្តើម</translation>
+    </message>
+    <message>
+        <location filename="../gomoku/src/widget/buttonfunction/btstartpause.cpp" line="152"/>
+        <source>Continue</source>
+        <translation>ຕໍ່ໄປ</translation>
+    </message>
+    <message>
+        <location filename="../gomoku/src/widget/buttonfunction/btstartpause.cpp" line="152"/>
+        <source>Pause</source>
+        <translation>ក្រោមកម្រិត</translation>
+    </message>
+</context>
+<context>
+    <name>Buttonagain</name>
+    <message>
+        <location filename="../gomoku/src/widget/resultpopup/buttonagain.cpp" line="140"/>
+        <source>Play Again</source>
+        <translation>ຫຼ຀ນອີກຄັ໬ນີ້</translation>
+    </message>
+</context>
+<context>
+    <name>Buttonrest</name>
+    <message>
+        <location filename="../gomoku/src/widget/resultpopup/buttonrest.cpp" line="159"/>
+        <source>Have a Rest</source>
+        <translation>ស្លាប់ផ្អែក</translation>
+    </message>
+</context>
+<context>
+    <name>CancelButton</name>
+    <message>
+        <location filename="../gomoku/src/widget/exitdialog/cancelbutton.cpp" line="116"/>
+        <source>Keep Playing</source>
+        <translation>ຕໍ່ຫຼຸດລື່ງ</translation>
+    </message>
+</context>
+<context>
+    <name>Determinebutton</name>
+    <message>
+        <location filename="../gomoku/src/widget/selectchess/determinebutton.cpp" line="106"/>
+        <source>OK</source>
+        <translation>ត្រូវ</translation>
+    </message>
+</context>
+<context>
+    <name>ExitButton</name>
+    <message>
+        <location filename="../gomoku/src/widget/exitdialog/exitbutton.cpp" line="115"/>
+        <source>Exit</source>
+        <translation>ចាក់ចេញ</translation>
+    </message>
+</context>
+<context>
+    <name>ExitLabel</name>
+    <message>
+        <location filename="../gomoku/src/widget/exitdialog/exitlabel.cpp" line="17"/>
+        <source>Are you sure you want to exit the game?</source>
+        <translation>តើអ្នកបានប្រាកដថាគូលុកចេញពីលំហ័រនេះដោយសURE?</translation>
+    </message>
+</context>
+<context>
+    <name>GomokuWindow</name>
+    <message>
+        <location filename="../gomoku/src/main.cpp" line="54"/>
+        <source>Gomoku</source>
+        <translation>gomoku</translation>
+    </message>
+    <message>
+        <location filename="../gomoku/src/main.cpp" line="56"/>
+        <source>Gomoku is a small chess game for two players.</source>
+        <translation>Gomokuជាលំហ័រផ្កាយតូចសម្រាប់អ្នកលេងពីរនាក់។</translation>
+    </message>
+</context>
+<context>
+    <name>PlayingScreen</name>
+    <message>
+        <location filename="../gomoku/src/widget/playingscreen/playingscreen.cpp" line="113"/>
+        <source>I am thinking...</source>
+        <translation>ຂ້ອນກຳລັງຄິດກຳນຳ...</translation>
+    </message>
+    <message>
+        <location filename="../gomoku/src/widget/playingscreen/playingscreen.cpp" line="116"/>
+        <source>Place your chess piece...</source>
+        <translation>តាំងទុកតូចរបស់អ្នកទី...</translation>
+    </message>
+    <message>
+        <location filename="../gomoku/src/widget/playingscreen/playingscreen.cpp" line="186"/>
+        <source>Welcome</source>
+        <translation>សួរ</translation>
+    </message>
+    <message>
+        <location filename="../gomoku/src/widget/playingscreen/playingscreen.cpp" line="99"/>
+        <source>Game Over!</source>
+        <translation>លំហ័របញ្ចប់!</translation>
+    </message>
+</context>
+<context>
+    <name>Resultinfo</name>
+    <message>
+        <location filename="../gomoku/src/widget/resultpopup/resultinfo.cpp" line="34"/>
+        <source>You did great!</source>
+        <translation>អ្នកធ្វើល្អណាស់!</translation>
+    </message>
+    <message>
+        <location filename="../gomoku/src/widget/resultpopup/resultinfo.cpp" line="36"/>
+        <source>Game Over</source>
+        <translation>លំហ័របញ្ចប់</translation>
+    </message>
+</context>
+<context>
+    <name>Selectinfo</name>
+    <message>
+        <location filename="../gomoku/src/widget/selectchess/selectinfo.cpp" line="35"/>
+        <source>Play as</source>
+        <translation>លេងជារួមជាមួយ</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/desktop/desktop_lo.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="lo">
+<context>
+    <name>desktop</name>
+    <message>
+        <location filename="Desktop Entry]Name" line="0"/>
+        <source>Gomoku</source>
+        <translation>ប្រាំពិច</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]GenericName" line="0"/>
+        <source>Gomoku</source>
+        <translation>ប្រាំពិច</translation>
+    </message>
+</context>
+</TS>
